7010d9e50c19456c530fc3e4d9867cad38c9ac1d
Maleesha Rukshan <abeywardenamr@gmail.com>
2026-06-27 11:40:34 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -40,8 +40,13 @@ userSchema.pre('save', async function (next) {
     return next();
   }
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Method to verify passwords on login
